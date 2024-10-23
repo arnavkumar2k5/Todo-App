2a1928a314fabb271231c913482903b2d48e5912
arnavkumar2k5 <144643023+arnavkumar2k5@users.noreply.github.com>
2024-10-24 00:54:01 +0530

    change in file structure of landing page

---
+++ src/container/LandingContainer/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+import TodoButton from "../../components/base/TodoButton"
+
+function LandingContainer() {
+
+    return (
+        <div>
+            <div className="flex flex-col justify-center items-center text-center mt-20 md:mt-40 m-5 md:w-[40%] shadow-2xl bg-white p-10 md:m-auto gap-5">
+                <div className="font-bold text-3xl">Welcome to TodoApp!</div>
+                <div>Ready to simplify your life, one task at a time? Let's make your day more productive and organized!</div>
+                <div className="flex gap-2">
+                        <Link to="/signUp">
+                    <TodoButton className="hover:bg-black">
+                        Get Started
+                    </TodoButton>
+                        </Link>
+                        <Link to='/signIn'>
+                    <TodoButton className="bg-white text-black hover:bg-slate-200">Already have an account? Sign In</TodoButton>
+                        </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingContainer;
