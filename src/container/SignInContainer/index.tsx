8f07914bea9f2beff9065afb8c5115bd702082a1
arnavkumar2k5 <144643023+arnavkumar2k5@users.noreply.github.com>
2024-10-26 15:47:34 +0530

    Changes in the Authentication

--- src/container/SignInContainer/index.tsx
+++ src/container/SignInContainer/index.tsx
@@ -1,41 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import SignInView from '../../view/SignInView';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../contexts/AuthContext.tsx';
+import React, { useEffect, useState } from "react";
+import SignInView from "../../view/SignInView";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 const SignInContainer: React.FC = () => {
-    const { signIn, isAuthenticated } = useAuth();
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    
-    useEffect(() => {
-        if (isAuthenticated()) {
-            navigate('/home'); 
-        }
-    }, [isAuthenticated, navigate]);
+  const { signIn, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSignIn = async () => {
-        signIn(email, password);
+  useEffect(() => {
+    if (isAuthenticated()) {
+      navigate("/home");
+    }
+  }, [isAuthenticated, navigate]);
 
-        if (isAuthenticated()) { 
-            navigate('/home');
-        } else {
-            console.log("Failed to login. Check credentials.");
-        }
-    };
+  const handleSignIn = async () => {
+    signIn(email, password);
 
-    return (
-        <div>
-            <SignInView 
-                email={email}
-                password={password}
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleClick={handleSignIn}
-            />
-        </div>
-    );
+    if (isAuthenticated()) {
+      navigate("/home");
+    } else {
+      console.log("Failed to login. Check credentials.");
+    }
+  };
+
+  return (
+    <div>
+      <SignInView
+        email={email}
+        password={password}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        handleClick={handleSignIn}
+      />
+    </div>
+  );
 };
 
 export default SignInContainer;
--- src/container/SignUpContainer/index.tsx
+++ src/container/SignUpContainer/index.tsx
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import SignUpView from '../../view/SignUpView';
-import { useAuth } from '../../contexts/AuthContext.tsx';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import SignUpView from "../../view/SignUpView";
+import { useAuth } from "../../contexts/AuthContext";
 
 const SignUpContainer: React.FC = () => {
-    const { signUp, isAuthenticated } = useAuth(); 
-    const navigate = useNavigate();
-    const [name, setName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+  const { signUp, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    useEffect(() => {
-        if (isAuthenticated()) {
-            navigate('/home'); 
-        }
-    }, [isAuthenticated, navigate]);
+  useEffect(() => {
+    if (isAuthenticated()) {
+      navigate("/home");
+    }
+  }, [isAuthenticated, navigate]);
 
-    const handleSignUp = async () => {
-        signUp(email, password); 
+  const handleSignUp = async () => {
+    signUp(email, password);
 
-        if (isAuthenticated()) {
-            navigate('/home'); 
-        } else {
-            console.log("Failed to sign up. Please try again.");
-        }
-    };
+    if (isAuthenticated()) {
+      navigate("/home");
+    } else {
+      console.log("Failed to sign up. Please try again.");
+    }
+  };
 
-    return (
-        <div>
-            <SignUpView 
-                name={name} 
-                setName={setName} 
-                email={email} 
-                password={password} 
-                setEmail={setEmail} 
-                setPassword={setPassword} 
-                handleClick={handleSignUp} 
-            />
-        </div>
-    );
+  return (
+    <div>
+      <SignUpView
+        name={name}
+        setName={setName}
+        email={email}
+        password={password}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        handleClick={handleSignUp}
+      />
+    </div>
+  );
 };
 
 export default SignUpContainer;
