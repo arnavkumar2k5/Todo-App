868917685d142d6b83c575375bf6c87721448ccf
arnavkumar2k5 <144643023+arnavkumar2k5@users.noreply.github.com>
2024-11-04 18:42:53 +0530

    changes in HomeContainer

--- src/container/HomeContainer/index.tsx
+++ src/container/HomeContainer/index.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import FormInput from '../../components/custom/Form';
-import FormList from '../../components/custom/FormList';
 import { useDispatch, useSelector } from 'react-redux';
 import { editTodo, toggleTodoCompletion, removeTodo, addTodo } from '../../store/TodoSlice';
-import { AppDispatch, RootState } from '../../store/store'; 
+import { AppDispatch, RootState } from '../../store/store';
+import Homeview from '../../view/HomeView';
 
 interface TodoItem {
   id: string;
@@ -17,14 +16,13 @@ const HomeContainer: React.FC = () => {
   const [editText, setEditText] = useState<string>('');
 
   const todos = useSelector((state: RootState) => state.todos.todos);
-  
   const dispatch = useDispatch<AppDispatch>();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (input.trim()) {
       dispatch(addTodo(input));
-      setInput(''); 
+      setInput('');
     }
   };
 
@@ -43,17 +41,17 @@ const HomeContainer: React.FC = () => {
   };
 
   const handleDeleteClick = (id: string) => {
-    dispatch(removeTodo(id)); 
+    dispatch(removeTodo(id));
   };
-
 
   return (
     <div className='m-3 md:w-[80%] md:m-auto md:mt-10 shadow-2xl bg-gray-700 p-5 rounded-xl mb-80 md:mb-72 mt-10 pb-10'>
-      <div className='text-3xl mb-5 md:text-7xl font-semibold text-center md:mb-10 '><i>TODO - APP</i></div>
+      <div className='text-3xl mb-5 md:text-7xl font-semibold text-center md:mb-10'><i>TODO - APP</i></div>
       <div className='border-b border-gray-300 mb-6 w-3/4 m-auto'></div>
-    <div>
-      <FormInput input={input} setInput={setInput} handleSubmit={handleSubmit} />
-      <FormList
+      <Homeview
+        input={input}
+        setInput={setInput}
+        handleSubmit={handleSubmit}
         todos={todos}
         editingTodoId={editingTodoId}
         editText={editText}
@@ -63,7 +61,6 @@ const HomeContainer: React.FC = () => {
         handleCheckboxChange={handleCheckboxChange}
         handleDeleteClick={handleDeleteClick}
       />
-    </div>
     </div>
   );
 };
